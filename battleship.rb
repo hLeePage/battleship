b591d52ab1a676e5a3af006e63b5c42db3f4018d
hLeePage <hleepage@yahoo.com>
2015-10-12 13:37:20 -0500

    Add Ship class

--- battleship.rb
+++ battleship.rb
@@ -1,21 +1,9 @@
-class Player
-  attr_accessor :name
-  def initialize name = "Dave"
-    @name = name
-  end
-end
+require_relative 'player'
+require_relative 'ship'
 
-class HumanPlayer < Player
-  attr_accessor :name
-  def initialize name = "Dave"
-    @name = name
-  end
-end
 
-class ComputerPlayer < Player
-  attr_accessor :name
-  def initialize 
-    @name = "HAL 9000"
-  end
-end
+
+
+
+
 
---
+++ player.rb
@@ -0,0 +1,20 @@
+class Player
+  attr_accessor :name
+  def initialize name = "Dave"
+    @name = name
+  end
+end
+
+class HumanPlayer < Player
+  attr_accessor :name
+  def initialize name = "Dave"
+    @name = name
+  end
+end
+
+class ComputerPlayer < Player
+  attr_accessor :name
+  def initialize 
+    @name = "HAL 9000"
+  end
+end
