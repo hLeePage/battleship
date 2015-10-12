d0fb917e3d5665f01f82bcf6bd06c1cb9446bce7
hLeePage <hleepage@yahoo.com>
2015-10-12 15:38:08 -0500

    Add Ship.place and Ship.covers?

--- ship.rb
+++ ship.rb
@@ -5,21 +5,23 @@ class Ship
     @length = length
   end
 
-  occupied_spaces = []
-
   def place(x, y, horizontal)   
     if horizontal == true
       #generate a range for the x component
       stop = x + length - 1
       x_range = (x..stop).to_a
-      coordinates = x_range.map { |x| [x,y]}
+      @coordinates = x_range.map { |x| [x,y]}
     elsif horizontal == false
     #generate a range for the y component
       stop = x + length - 1
       y_range = (y..stop).to_a
-      coordinates = y_range.map { |y| [x,y]}
+      @coordinates = y_range.map { |y| [x,y]}
     end
-    return coordinates
+    @coordinates
+  end
+
+  def covers?(x, y)
+    return @coordinates.include?([x, y])
   end
 
 
