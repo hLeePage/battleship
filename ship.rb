3ebfbf9cfbfbaab9b01c6e3543436fef47ff9b03
hLeePage <hleepage@yahoo.com>
2015-10-12 15:26:00 -0500

    Add a Ship.place method.

--- ship.rb
+++ ship.rb
@@ -1,2 +1,26 @@
 class Ship
+  attr_accessor :length
+
+  def initialize length
+    @length = length
+  end
+
+  occupied_spaces = []
+
+  def place(x, y, horizontal)   
+    if horizontal == true
+      #generate a range for the x component
+      stop = x + length - 1
+      x_range = (x..stop).to_a
+      coordinates = x_range.map { |x| [x,y]}
+    elsif horizontal == false
+    #generate a range for the y component
+      stop = x + length - 1
+      y_range = (y..stop).to_a
+      coordinates = y_range.map { |y| [x,y]}
+    end
+    return coordinates
+  end
+
+
 end
