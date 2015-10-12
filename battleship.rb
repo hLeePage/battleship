a77dd1c717fb514ace20928ec3530cc580abdb1a
hLeePage <hleepage@yahoo.com>
2015-10-12 13:27:57 -0500

    Players have default names.

--- battleship.rb
+++ battleship.rb
@@ -1,13 +1,21 @@
 class Player
+  attr_accessor :name
+  def initialize name = "Dave"
+    @name = name
+  end
 end
 
 class HumanPlayer < Player
   attr_accessor :name
-  def initialize name 
+  def initialize name = "Dave"
     @name = name
   end
 end
 
 class ComputerPlayer < Player
+  attr_accessor :name
+  def initialize 
+    @name = "HAL 9000"
+  end
 end
 
