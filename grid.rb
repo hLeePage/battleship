a80e837e907446a7394d9992f2c28f3f7f345dcf
hLeePage <hleepage@yahoo.com>
2015-10-14 16:47:24 -0500

    Progress on grid functions.

--- grid.rb
+++ grid.rb
@@ -8,20 +8,17 @@ class Grid
   end
 
   def display
-  puts "    1   2   3   4   5   6   7   8   9   10"
-  puts "-----------------------------------------"
-  puts "A |   |   |   |   |   |   |   |   |   |   |"
-  puts "B |   |   |   |   |   |   |   |   |   |   |"
-  puts "C |   |   |   |   |   |   |   |   |   |   |"
-  puts "D |   |   |   |   |   |   |   |   |   |   |"
-  puts "E |   |   |   |   |   |   |   |   |   |   |"
-  puts "F |   |   |   |   |   |   |   |   |   |   |"
-  puts "G |   |   |   |   |   |   |   |   |   |   |"
-  puts "H |   |   |   |   |   |   |   |   |   |   |"
-  puts "I |   |   |   |   |   |   |   |   |   |   |"
-  puts "J |   |   |   |   |   |   |   |   |   |   |"
-  puts  "-----------------------------------------"
+    
+    puts "    1   2   3   4   5   6   7   8   9   10"
+    puts border
+    ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"].each do |ltr|
+      puts "#{ltr} |"
+    end
+    puts border
   end
 
+  def border
+    "  -----------------------------------------"
+  end
 
-end
+end 
